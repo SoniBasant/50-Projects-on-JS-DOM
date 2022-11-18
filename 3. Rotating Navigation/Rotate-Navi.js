d76a47ee7f2e8419ee32892ddd08824861b005c0
SoniBasant <94775800+SoniBasant@users.noreply.github.com>
2022-11-18 12:44:34 +0530

    Update Rotate-Navi.js
    Add concepts and comments.

--- 3. Rotating Navigation/Rotate-Navi.js
+++ 3. Rotating Navigation/Rotate-Navi.js
@@ -1,7 +1,18 @@
+
+// 3. Rotating Navigation
+
+// Important concepts for the project
+
+// 1. getElementById()
+// 2. querySelector()
+// 3. addEventListener('click')
+
+// variables
 const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
+// change style on event of click
 open.addEventListener('click', () => container.classList.add('show-nav'))
 // adding a class 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
