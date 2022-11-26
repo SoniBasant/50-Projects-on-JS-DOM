22b3faf45359bb495fc25167a0e253f739734398
SoniBasant <94775800+SoniBasant@users.noreply.github.com>
2022-11-26 16:40:34 +0530

    Add files via upload

---
+++ 11. Event Keycodes/eventKeycodes.js
@@ -0,0 +1,50 @@
+
+// 11. Event Key Codes
+
+// IMportant JS and CSS concepts for project
+
+// 1. getElementById()
+// 2. addEventListener('keydown')
+// 3. template literals for variables
+// 4. key, keycode and code for any keyboard key
+// 5. @import for font from fontawesome
+
+// ---------------------------------------------
+// to check any key in the console
+
+// window.addEventListener('keydown', (event) => {
+//     console.log(event);
+// });
+
+// variables
+
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', (event) => {
+    insert.innerHTML = `
+    <div class="key">
+    ${event.key === ' ' ? 'Space' : event.key}
+    <small>event.key</small>
+    </div>
+
+    <div class="key">
+        ${event.keyCode}
+        <small>event.keyCode</small>
+    </div>
+
+    <div class="key">
+        ${event.code}
+        <small>event.code</small>
+    </div>
+    `
+    // in the first "key"
+    // {condition then result1 else result2}
+    // condition -> event.key === ' '
+    // then -> ?
+    // result1 -> 'Space'
+    // else -> :
+    // result -> event.key
+
+    // in second "key"
+    // this is deprecated, so line through
+});
