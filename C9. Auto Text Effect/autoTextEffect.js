1ac0c137dcbb87c3e6bbe6648f368d8348aaeae5
SoniBasant <94775800+SoniBasant@users.noreply.github.com>
2022-12-16 18:30:08 +0530

    Add files via upload

---
+++ C9. Auto Text Effect/autoTextEffect.js
@@ -0,0 +1,37 @@
+
+// 30. Auto Text Effect
+
+// Important concepts for the project
+
+// 1. getElementById()
+// 2. .innerText
+// 3. slice()
+// 4. setTimeout()
+// 5. addEventListener('input')
+
+
+// variables
+
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'We Love Programming!';
+
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx);
+
+    idx++
+
+    // to keep going animation
+    if(idx > text.length) {
+        idx = 1;
+    }
+
+    setTimeout(writeText, speed);
+}
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value);
