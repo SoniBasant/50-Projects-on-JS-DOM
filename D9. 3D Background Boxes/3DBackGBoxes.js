d995b872ea61e37a151208d8ba7d2e98fb09a230
SoniBasant <94775800+SoniBasant@users.noreply.github.com>
2022-12-29 19:48:29 +0530

    Add files via upload

---
+++ D9. 3D Background Boxes/3DBackGBoxes.js
@@ -0,0 +1,34 @@
+
+// 40. 3D Background Boxes
+
+// Important concepts for the project
+
+// 1. getElementById()
+// 2. for loop
+// 3. createElement()
+// 4. classList > add and toggle
+// 5. element.style.backgroundPosition
+// 6. template literals
+// 7. appendChild()
+// 8. addEventListener('click')
+
+// variables
+
+const boxesContainer = document.getElementById('boxes');
+const btn = document.getElementById('btn');
+
+function createBoxes() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+            boxesContainer.appendChild(box);
+            // console.log(box);
+        }
+    }
+}
+
+createBoxes();
+
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'));
