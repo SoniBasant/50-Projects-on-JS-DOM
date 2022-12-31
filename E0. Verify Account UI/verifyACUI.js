80c39b1c3ccfa286f60fd939002f692e8d66d5ce
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2022-12-31 19:40:00 +0530

    Add files via upload

---
+++ E0. Verify Account UI/verifyACUI.js
@@ -0,0 +1,41 @@
+
+// 41. Verify Account UI
+
+// Important concepts for the project
+
+// 1. -webkit-outer-spin-button CSS pseudo-element to style input element
+// 2. -webkit-inner-spin-button
+// 3. querySelectorAll()
+// 4. focus()
+// 5. addEventListener('keydown')
+// 6. if...else
+// 7. setTimeout(function, time in ms)
+// 8. element.style.borderColor
+
+// variables
+
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+    code.addEventListener('keydown', (e) => {
+        if(e.key >= 0 && e.key <=9) {
+            // delete any previous value at the place by assigning its value to empty string
+            codes[idx].value = '';
+            // wait for 10ms then shift focus to next part
+            setTimeout(() => codes[idx + 1].focus(), 10);
+            // add border color for active code
+            codes[idx].style.borderColor = '#3498db';
+            // but if you make a mistake then correct it by pressing backspace
+        } else if(e.key === 'Backspace') {
+            // so if you press backspace, focus will shift to the previous place after 10ms
+            setTimeout(() => codes[idx - 1].focus(), 10);
+            // add border color for going back place
+            if(idx === (codes.length -1)) {
+                codes[idx].style.borderColor = '#f48004';
+            }
+            codes[idx].style.borderColor = '#f48004';            
+        }
+    });
+});
