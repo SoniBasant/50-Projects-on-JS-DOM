00379d2899491068b0814fd1a15df9eebaa4afa6
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-01-01 18:08:10 +0530

    Update Expan-cards.js

--- A0. Expanding Cards/Expan-cards.js
+++ A0. Expanding Cards/Expan-cards.js
@@ -2,28 +2,29 @@
 // 1. Expanding Cards
 
 // Important concepts for this project
+
 // 1. querySelectorAll()
 // 2. forEach() loop
-// 3. addEventListener "click"
+// 3. addEventListener() > click
 // 4. classList.add()
 // 5. classList.remove()
 
-const panels=document.querySelectorAll('.panel') 
+const panels = document.querySelectorAll('.panel'); 
 //without "All" querySelector select only first one
 
 panels.forEach((panel) => { //add loop for each
     panel.addEventListener('click', () => { 
         //add event listener-> click
-        removeActiveClasses()//to deactive panel previously active
-        panel.classList.add('active')
-    }) //but by only doing this don't deactive previously active one. SO
+        removeActiveClasses();//to deactive panel previously active
+        panel.classList.add('active');
+    }); //but by only doing this don't deactive previously active one. SO
 })
 //here, we make a function to remove active class and
 //place it before active
 function removeActiveClasses() {
     panels.forEach(panel => { //loop through it
-        panel.classList.remove('active')
-    })
+        panel.classList.remove('active');
+    });
 }
 
 //add styles in style.css-> transition and delay
