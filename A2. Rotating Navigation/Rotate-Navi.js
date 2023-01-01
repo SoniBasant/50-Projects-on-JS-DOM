49752c6ca98c293be3fd41308771765811177101
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-01-01 18:12:06 +0530

    Update Rotate-Navi.js

--- A2. Rotating Navigation/Rotate-Navi.js
+++ A2. Rotating Navigation/Rotate-Navi.js
@@ -6,14 +6,16 @@
 // 1. getElementById()
 // 2. querySelector()
 // 3. addEventListener('click')
+// 4. classList.add()
+// 5. classList.remove()
 
 // variables
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const container = document.querySelector('.container');
 
-// change style on event of click
-open.addEventListener('click', () => container.classList.add('show-nav'))
+// change style on the event of click
+open.addEventListener('click', () => container.classList.add('show-nav'));
 // adding a class 
-close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', () => container.classList.remove('show-nav'));
 // removing a class 
