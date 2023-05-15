c2d1a50d449a704c6459c3f06e3110b8b13c78ea
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-05-15 10:31:05 +0530

    Change messages and delete commented code

--- C6. Toast Notification/toastNotification.js
+++ C6. Toast Notification/toastNotification.js
@@ -1,114 +1,3 @@
-
-// 27. Toast Notification
-
-// Important concepts for the project
-
-// 1. getElementById()
-// 2. arrays
-// 3. addEventListener('click')
-// 4. createElement
-// 5. classList.add
-// 6. appendChild
-// 7. innerText
-// 8. setTimeout()
-// 9. Math.floor()
-// 10. Math.random()
-// 11. ternary operator
-// 12. arraow function
-// 13. element.remove()
-// 14. :root
-// 15. @import
-
-// PROGRAM 1
-
-// // RANDOM MESSAGES
-
-// // variables
-
-// const btn = document.getElementById('button');
-// const toasts = document.getElementById('toasts');
-
-// // default messages
-// const messages = [
-//     'Message One',
-//     'Message Two',
-//     'Message Three',
-//     'Message Four',
-//     'Message Five',
-//     'Message Six',
-// ]
-
-// btn.addEventListener('click', () => createNotification());
-
-// function createNotification() {
-
-//     const notif = document.createElement('div');
-//     notif.classList.add('toast');
-//     toasts.appendChild(notif);
-//     notif.innerText = getRandomMessage();
-
-//     // we can not place this remove function outside
-//     // createNoti funtion because 'notif' is local
-//     setTimeout(() => {
-//         notif.remove()
-//     }, 3000);
-// }
-
-// function getRandomMessage() {
-//     return messages[Math.floor(Math.random() * messages.length)];
-// }
-
-
-// --------------------------------------------------------------------
-
-// PROGRAM 2
-
-// // FIXED MESSAGE
-
-// // variables
-
-// const btn = document.getElementById('button');
-// const toasts = document.getElementById('toasts');
-
-// // default messages
-// const messages = [
-//     'Message One',
-//     'Message Two',
-//     'Message Three',
-//     'Message Four',
-//     'Message Five',
-//     'Message Six',
-// ]
-
-// btn.addEventListener('click', () => createNotification('This is invalid data', 'success'));
-
-// function createNotification(message = null, type = null) {
-
-//     const notif = document.createElement('div');
-//     notif.classList.add('toast');
-//     notif.classList.add(type ? type : 'info');
-    
-//     toasts.appendChild(notif);
-//     notif.innerText = message ? message : getRandomMessage();
-
-//     // we can not place this remove function outside
-//     // createNoti funtion because 'notif' is local
-//     setTimeout(() => {
-//         notif.remove()
-//     }, 3000);
-// }
-
-// function getRandomMessage() {
-//     return messages[Math.floor(Math.random() * messages.length)];
-// }
-
-
-// --------------------------------------------------------------------
-
-// PROGRAM 3
-
-// RANDOM MESSAGE AND RANDOM COLOUR
-
 // variables
 
 const btn = document.getElementById('button');
@@ -116,12 +5,13 @@ const toasts = document.getElementById('toasts');
 
 // default messages
 const messages = [
-    'Message One',
-    'Message Two',
-    'Message Three',
-    'Message Four',
-    'Message Five',
-    'Message Six',
+    'The boy who survived',
+    'The Choosen One',
+    'I open at close',
+    'Two Phoenix feathers',
+    'shss Ssshass shasah',
+    'ALWAYS',
+    'Golden Trinity and Silver Trinity',
 ]
 
 const types = ['info', 'success', 'error']
