b468614273239c0bbe094a7596846fa4761ae2f7
Basant Soni <94775800+SoniBasant@users.noreply.github.com>
2023-06-12 11:54:55 +0530

    Change output style
    Instead of many images with one theme, I changed the code to have one image per theme and get many images.
    
    Using random and sort method, theme placement will change on every page loading.

--- E7. Random Image Feed/randomImgFeed.js
+++ E7. Random Image Feed/randomImgFeed.js
@@ -13,34 +13,62 @@
 
 // variables
 const container = document.querySelector('.container');
-const unsplashURL = 'https://source.unsplash.com/collection/1298463/';
+
+// // const unsplashURL = 'https://source.unsplash.com/collection/1298463/';
 // const unsplashURL = 'https://source.unsplash.com/random/';
-const rows = 5;
-// and columns will be 3
-display();
-function display() {
-    for(let i = 0; i < rows * 3; i++) {
-        const img = document.createElement('img');
-        img.src = `${unsplashURL}${getRandomSize()}`;
-        // img.src = `${unsplashURL}${getRandomSize()}?food`;
-        container.appendChild(img);
-    }
+// const rows = 5;
+// // and columns will be 3
+// display();
+// function display() {
+//     for(let i = 0; i < rows * 3; i++) {
+//         const img = document.createElement('img');
+//         // img.src = `${unsplashURL}${getRandomSize()}`;
+//         img.src = `${unsplashURL}${getRandomSize()}?shiv`;
+//         container.appendChild(img);
+//     }
+// }
+
+
+// // each random size will give different image
+// // here we are getting random size b/w 300 and 310
+
+
+// // random numbers b/w 200 and 300
+// function getRandomNum() {
+//     return Math.floor(Math.random() * 100) + 200;
+// }
+
+// // random size
+// function getRandomSize() {
+//     return `${getRandomNum()}x${getRandomNum()}`;
+// }
+
+const unsplashURL = [
+    'https://source.unsplash.com/random/200x220/?food',
+    'https://source.unsplash.com/random/210x220/?mute,and,pastel',
+    'https://source.unsplash.com/random/200x220/?baby',
+    'https://source.unsplash.com/random/210x220/?baby',
+    'https://source.unsplash.com/random/200x220/?spring',
+    'https://source.unsplash.com/random/210x220/?spring',
+    'https://source.unsplash.com/random/200x220/?girl',
+    'https://source.unsplash.com/random/210x220/?girl',
+    'https://source.unsplash.com/random/200x220/?summer',
+    'https://source.unsplash.com/random/210x220/?blue',
+    'https://source.unsplash.com/random/200x220/?car',
+    'https://source.unsplash.com/random/210x220/?eco',
+    'https://source.unsplash.com/random/200x220/?space',
+    'https://source.unsplash.com/random/210x220/?life,in,the,deep',
+    'https://source.unsplash.com/random/200x220/?luxury',
+    'https://source.unsplash.com/random/210x220/?ice,creme',
+    'https://source.unsplash.com/random/210x220/?snow',
+    'https://source.unsplash.com/random/210x220/?landscape',
+];
+shuffleImg = unsplashURL.sort(() => Math.random() - 0.5);
+const rows = 6;
+const columns = 3;
+for(let i = 0; i < rows*columns; i++) {
+    const img = document.createElement('img');
+    
+    img.src = shuffleImg[i]
+    container.appendChild(img);
 }
-
-
-// each random size will give different image
-// here we are getting random size b/w 300 and 310
-
-
-// random numbers b/w 200 and 300
-function getRandomNum() {
-    return Math.floor(Math.random() * 100) + 200;
-}
-// console.log(getRandomSize());
-
-// random size
-function getRandomSize() {
-    return `${getRandomNum()}x${getRandomNum()}`;
-}
-
-// console.log(getRandomSize());
